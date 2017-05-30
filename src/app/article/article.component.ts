c1d783fb2939dab08ea0cdda9adc565560b43d65
Francisco Axel Lanuza Rojas <axel.lanuza@definityfirst.com>
2017-05-30 10:35:41 -0500

    Added article component and html

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
+  public votes: number;
+  public title: string;
+  public link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 4';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
 
   ngOnInit() {
+  }
+
+  public voteUp() {
+    this.votes += 1;
+  }
+
+  public voteDown() {
+    this.votes -= 1;
   }
 
 }
